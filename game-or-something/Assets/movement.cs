11282aa98bad989059b7fad06ec1c453b23ac799
Evelyn36 <thunderslashgamez@gmail.com>
2024-06-07 15:58:59 -0500

    block movement
    you can chnage movement speed under movement script

--- game-or-something/Assets/movement.cs
+++ game-or-something/Assets/movement.cs
@@ -5,25 +5,58 @@ using UnityEngine;
 public class movement : MonoBehaviour
 {
 
-    private Rigidbody2D player;
+    private Transform player;
+    private BoxCollider2D hitbox;
+    public float movementSpeed;
+    private float speed;
+
+    private bool colliding;
 
     // Start is called before the first frame update
     void Start()
     {
-        player = GetComponent<Rigidbody2D>();
+        player = GetComponent<Transform>();
+        hitbox = GetComponent<BoxCollider2D>();
+        speed = movementSpeed / 100;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        colliding = true;
+        Debug.Log("hello");
+    }
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        colliding = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        if (!(colliding))
         {
-            player.AddForce(new Vector2(-200, 0));
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                player.transform.position = new Vector2(player.transform.position.x - speed, player.transform.position.y);
+
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                player.transform.position = new Vector2(player.transform.position.x + speed, player.transform.position.y);
+            }
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                player.transform.position = new Vector2(player.transform.position.x, player.transform.position.y + speed);
+
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                player.transform.position = new Vector2(player.transform.position.x, player.transform.position.y - speed);
+            }
 
         }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            player.AddForce(new Vector2(200, 0));
-        }
+
     }
 }
