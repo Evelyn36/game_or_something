e1e2c5224cf5b38eb5a3e5ee76a044a9424c3eff
Evelyn36 <thunderslashgamez@gmail.com>
2024-06-07 15:19:56 -0500

    falling box???

---
+++ game-or-something/Assets/movement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+
+    private Rigidbody2D player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            player.AddForce(new Vector2(-2, 0));
+
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            player.AddForce(new Vector2(2, 0));
+        }
+    }
+}
